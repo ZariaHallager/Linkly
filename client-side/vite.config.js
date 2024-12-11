2b313da51d209032e15ea75b36864b39aa97c3ce
Zaria Hallager <120356303+ZariaHallager@users.noreply.github.com>
2024-12-11 07:49:27 -0500

    adding to vite config

--- client-side/vite.config.js
+++ client-side/vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    host: true, // Allows external access (e.g., from Render's environment)
+    port: 3000, // Specify the port Render expects (default: 3000)
+  },
+});
