e41f9c940477b1ec116ea4a69a862365611e7a31
Zaria Hallager <120356303+ZariaHallager@users.noreply.github.com>
2024-12-11 12:16:56 -0500

    fixing sign in page

--- client-side/vite.config.js
+++ client-side/vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   server: {
     host: true, // Allows external access (e.g., from Render's environment)
-    port: 3000, // Specify the port Render expects (default: 3000)
+    port: 5000, // Specify the port Render expects (default: 3000)
   },
 });
